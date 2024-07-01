514c1f572cdf5aa2b3e4f6f41ca16565417b9db1
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-07-01 20:45:42 -0300

    quito front

--- demo-power-rangers-front/src/services/PowerRangerService.js
+++
@@ -1,8 +0,0 @@
-import httpClient from "../utils/HttpClient";
-
-export const getMyTop5PowerRangersSeasons = async () => {
-  const data = await httpClient
-    .get("/my-top-5-power-rangers-seasons")
-    .then((v) => v.data);
-  return data;
-};
