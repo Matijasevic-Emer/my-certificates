6153461cdc8ef215a73a27d30d077d4309d497bd
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-07-01 20:54:22 -0300

    cambio mock

--- demo-power-rangers-back/functions/topSeasons.js
+++ demo-power-rangers-back/functions/topSeasons.js
@@ -2,21 +2,21 @@ export const getMyTop5PowerRangersSeasons = () => {
   return [
     {
       season: 9,
-      title: "Power Rangers Time Force",
+      title: "Power Rangers Fuerza del tiempo",
       episodes: 40,
       firstReleased: "February 3, 2001",
       lastReleased: "November 17, 2001",
     },
     {
       season: 13,
-      title: "Power Rangers S.P.D.",
+      title: "Power Rangers la de los policias",
       episodes: 38,
       firstReleased: "February 5, 2005",
       lastReleased: "November 14, 2005",
     },
     {
       season: 6,
-      title: "Power Rangers in Space",
+      title: "Power Rangers en el espacio",
       episodes: 43,
       firstReleased: "February 6, 1998",
       lastReleased: "November 21, 1998",
